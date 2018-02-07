4c0c953bca5677cc09bdc6502c64ae8dc1933ccc
alexpm94 <j.alejandro.pena.m94@gmail.com>
2018-02-07 12:38:01 -0600

    Add example to the singleNumber task.

--- interviewPractice/singleNumber.py
+++ interviewPractice/singleNumber.py
@@ -1,5 +1,22 @@
+"""
+You are given an array of integers in which
+every element appears twice, except for one. 
+Find the element that only appears one time. 
+Your solution should have a linear runtime 
+complexity (O(n)). Try to implement it without 
+using extra memory.
+
+Example
+
+For nums = [2, 2, 1], the output should be
+singleNumber(nums) = 1.
+"""
+
 def singleNumber(nums):
     res = 0
     for num in nums:
         res^=num
     return res
+
+nums = [2, 2, 1]
+print(singleNumber(nums))
